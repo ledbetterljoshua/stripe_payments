496ef558c858850bde7be234162588602821aa66
Joshua Ledbetter <ledbetterljoshua@gmail.com>
2014-10-29 20:45:13 -0500

    changed styles for nav bars

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,6 +1,6 @@
 class PurchaseMailer < ActionMailer::Base
 	layout 'purchase_mailer'
-	 default from: "One Month Commerce <commerce@onemonth.com>"
+	 default from: "Hand & Word <hand@word.com>"
 
 	def purchase_receipt purchase
 		@purchase = Purchase
